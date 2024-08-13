34d682decee8e2a4254296179d96357202fb64f7
DylanBozarth <dylanbozarth@theluxergroup.com>
2024-08-13 13:35:11 -0700

    update

--- credentials.js
+++ credentials.js
@@ -1,8 +1,8 @@
 const Config = {
-    CLIENT_ID: 'dylan', // fill with your client id, it should be something like a name
-    CLIENT_SECRET: 'mVAlx0cO9qmfpOPxZ2RUuTotQ9Pwmx3C', // fill with your client secret, it should be a long string of numbers and letters
-    SDK_ENV: 'sandbox', // either sandbox, development, or production. Case sensitive, all lowercase.
-    MY_TOWER_ID: '0100000000000033', // fill with your tower id, it should be a 16 hexadecimal digit id
+    CLIENT_ID: '', // fill with your client id, it should be something like a name
+    CLIENT_SECRET: '', // fill with your client secret, it should be a long string of numbers and letters
+    SDK_ENV: '', // either sandbox, development, or production. Case sensitive, all lowercase.
+    MY_TOWER_ID: '', // fill with your tower id, it should be a 16 hexadecimal digit id
   };
   
   export default Config;
